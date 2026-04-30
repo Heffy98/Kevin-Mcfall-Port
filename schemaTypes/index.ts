f4a795493fea4d60b5fc2f0383313c0c745ddc0f
Heffy98 <qglover@icstars.org>
2026-04-30 17:16:24 -0500

    Update Sanity studio schema and config

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,36 @@
-export const schemaTypes = []
+import {defineType, defineField} from 'sanity'
+
+export const project = defineType({
+  name: 'project',
+  title: 'Portfolio Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'tag',
+      title: 'Tag',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'impact',
+      title: 'Impact',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    }),
+  ],
+})
+
+export const schemaTypes = [project]
